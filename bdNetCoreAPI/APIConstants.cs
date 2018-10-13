2fca8d645e32fc04c0fe7de5b3cd41216af8bbc4
Bret Dalldorf <bret.dalldorf@gmail.com>
2018-10-12 20:56:33 -0700

    feat(app): Created API Constants to reduce magic strings

---
+++ bdNetCoreAPI/APIConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bdNetCoreAPI
+{
+    public static class ApiConstants
+    {
+        public const string Title = "My API";
+        public const string ApiVersion = "1.0";
+        public const string ApiDescription = "A Simple API";
+        public const string ApiTermsOfService = "";
+        public const bool ValidateIssuer = true;
+        public const bool ValidateAudience = true;
+        public const bool ValidateLifetime = true;
+        public const bool ValidateIssuerSigningKey = true;
+        public const string ValidIssuer = "yourdomain.com";
+        public const string ValidAudience = "yourdomain.com";
+    }
+}
